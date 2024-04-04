893b19a1f6ce7bb5e698d28681611e274513f81c
dipronil <dipronil.das@techwens.in>
2024-04-04 12:07:30 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Products from '../components/Product'
+
+const Home = () => {
+  return (
+    <div>
+        <h2 className='heading'>Welcome to Redux toolkit</h2>
+        <Products/>
+    </div>
+  )
+}
+
+export default Home
